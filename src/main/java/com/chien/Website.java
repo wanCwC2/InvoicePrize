0d51ddc3f6207a925c55271d14e77b70ee0d518f
CWC2 <408570344@gapp.fju.edu.tw>
2021-06-28 16:54:50 +0800

    Website class: DEBUG draw day is on the 25th

--- src/main/java/com/chien/Website.java
+++ src/main/java/com/chien/Website.java
@@ -17,17 +17,22 @@ public class Website {
         int site = scan.nextInt();
         String timeNowYear = new SimpleDateFormat("yyyy").format(Calendar.getInstance().getTime());
         String timeNowMonth = new SimpleDateFormat("MM").format(Calendar.getInstance().getTime());
+        String timeNowDay = new SimpleDateFormat("dd").format(Calendar.getInstance().getTime());
         int yearNow = Integer.parseInt(timeNowYear)-1911;
-        int monthNow = Integer.parseInt(timeNowMonth);
+        int monthNow = 8;
+        int dayNow = 24;
         if (site/100 > yearNow
                 || (site/100 == yearNow && site%100 > monthNow)
                 || site%100 > 12
                 || site%100 < 0
-                || ((site%100)+1)%2 == 5
+                || ((site%100)+1)%2 != 0
                 || site/100 < 101
                 || (site/100 == 101 && site%100 <5)){
             return false;
-        }else{
+        }else if(dayNow < 25 && site%100 == monthNow){
+            System.out.println("尚未開獎");
+            return false;
+        } else{
             year = Integer.toString(site/100);
             if (site%100 < 10) {
                 month = "0" + Integer.toString(site % 100);
