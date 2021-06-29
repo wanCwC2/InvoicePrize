272a935f6ed589b3d93d97f2171245571891427c
CWC2 <408570344@gapp.fju.edu.tw>
2021-06-29 23:08:23 +0800

    repair JLabel using html

--- src/main/java/com/chien/Prize.java
+++ src/main/java/com/chien/Prize.java
@@ -18,16 +18,17 @@ public class Prize extends MachineFrame {
     }
 
     public void content(String year, String month){
-        frame();
-        label.setText("<html>以下是民國"+year+"年"
+        label.setText("");
+        label.setText("<html><body align=\"center\"><h1>民國"+year+"年"
                 +Integer.parseInt(month)
                 +"月至"+(Integer.parseInt(month)+1)
-                +"月的中獎號碼<br/>-------<br/>");
+                +"月的中獎號碼</h1><br></body></html>");
         CatchData cd = new CatchData();
         data = cd.get(year, month);
         String[] nameData = {"特別獎", "特獎", "頭獎A", "頭獎B", "頭獎C", "增開六獎", "增開六獎2"};
         for (int i = 0; i < data.size(); i++) {
-            label.setText(label.getText()+nameData[i]+"-------"+data.get(i)+"<br/>");
+            label.setText(label.getText().replaceAll("</body></html>","") +
+                    "<p>"+nameData[i]+"-------"+data.get(i)+"</p><br></body></html>");
         }
     }
 
@@ -43,20 +44,21 @@ public class Prize extends MachineFrame {
     }
 
     public void print(){
-        dispose();
-        frame();
-        label.setText("<html>以下是你的中獎發票<br/>-------<br/>");
+        label.setText("");
+        label.setText("<html><body align=\"center\"><h1 align=\"center\">你的中獎發票</h1></body></html>");
         if (enter.size() == 0){
-            label.setText(label.getText()+"完全沒中獎喔！");
+            label.setText(label.getText().replaceAll("</body></html>","") +"<p>完全沒中獎喔！</p></body></html>");
         }else{
             for (int i = 0; i < enter.size(); i++) {
-                label.setText(label.getText()+"你的發票號碼"+enter.get(i)+
-                        " 中"+winName.get(i)+
-                        " 得到獎金"+winBonus.get(i)+"元<br/>");
+                label.setText(label.getText().replaceAll("</body></html>","") +
+                        "<p>你的發票號碼"+enter.get(i) +
+                        " 中" + winName.get(i) +
+                        " 得到獎金" + winBonus.get(i)+"元</p></body></html>");
             }
         }
         if (total != 0) {
-            label.setText(label.getText()+"-------<br/>這次兌換發票的中獎總金額為" + total + "元");
+            label.setText(label.getText().replaceAll("</body></html>","") +
+                    "<h2>這次兌換發票的中獎總金額為" + total + "元</h2></body></html>");
         }
     }
 
